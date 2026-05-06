9197b52502c6c2fa2e9c4225d88bfdcdc8188ba6
MUHAMMAD SAAD <saadw7751@gmail.com>
2026-05-06 21:43:45 +0500

    chore: clean repo and apply gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.spendwise"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.spendwise"
+        applicationId = "com.saaddev.spendwise"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
